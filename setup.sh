c7705ef1beebb0a804b90db906ea2326dfb5c628
Daniel Lawson <daniellawson9999@gmail.com>
2019-11-12 14:39:35 -0500

    updated setup.sh

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# As a point of reference, the environment layout should be as follows
+# /home/$user/Projects/Rover-Arm-2019-2020 for the Boiler Robotic's Arm Github
+# /home/$user/catkin_workspace for the user's catkin catkin_workspace
+# By keeping this consistent across all development machines, it will make it
+# easier to keep track of things
+
+# Which folders should be symbolically_linked?
+folders_to_link=(
+    rover_arm
+    rover_arm_moveit_config
+    mr1718-arm-urdf_export
+)
+
+# Print heading
+echo "Setting up ROS packages for ground_station."
+
+# Get the catkin_workspace directory
+catkin_workspace_dir="catkin_workspace"
+catkin_workspace_path="$HOME/$catkin_workspace_dir"
+catkin_src_path="$catkin_workspace_path/src"
+
+# Get the rover software directory
+github_rover_repo_dir="Projects/Rover-Arm-2019-2020"
+github_rover_packages_path="$HOME/$github_rover_repo_dir/ros_packages"
+
+# Remove existing symbolic links if necessary
+symlinked_folders=$(find $catkin_src_path -maxdepth 1 -type l)
+if [ -z $symlinked_folders ]; then
+    echo "No symlinks to remove from catkin_workspace. Skipping."
+else
+    echo "Removing existing symlinks in catkin_workspace."
+    rm $symlinked_folders
+fi
+
+# Make the new symbolic link connections
+echo "Making new symlinks."
+for folder in ${folders_to_link[@]}; do
+    ln -s "$github_rover_packages_path/$folder" "$catkin_src_path/."
+    echo "Adding symlink for $folder."
+done
+
+# catkin_make so the new pacakges are available and re-source bash
+cd "$catkin_workspace_path"
+catkin_make
+
+source ~/.bashrc
+
+exit 0
